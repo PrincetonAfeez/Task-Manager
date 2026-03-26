c999058e0d0a38876135d61046a97775ea50da79
PrincetonAfeez <newlifewithprince@live.com>
2026-03-26 13:20:26 -0700

    Initialize task_manager.py and connect to local environment

---
+++ task_manager.py
@@ -0,0 +1,9 @@
+#Task Manger for handling asynchronous tasks in the application."
+
+import json
+import os
+from datetime import datetime
+
+# The name of our "database" file
+DATA_FILE = "tasks.json"
+
